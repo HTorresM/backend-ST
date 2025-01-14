b7eb9adf2856bffa1526f66e6ee86133df80dfc0
Hugo Torres <hogotoluca@hotmail.com>
2025-01-13 20:50:20 -0600

    Correccion de EncuentroConfig

--- src/main/java/com/sylen/SistemaTorneos/Config/EncuentrosConfig.java
+++ src/main/java/com/sylen/SistemaTorneos/Config/EncuentrosConfig.java
@@ -28,51 +28,34 @@ public class EncuentrosConfig {
     @Scheduled(cron = "0 0 0 * * ?") // Ejecuta todos los días a medianoche
     public void generarEncuentros() {
         List<Torneo> torneosCerrados = torneoApp.findAllCaducados(new Date());
-        List<List<Equipo>> torneosRegistrados = new ArrayList<>();
+        List<Equipo> torneoRegistrado;
         // TRAER LISTA DE EQUIPOS RELACIONADOS CON TORNEO
         for (Torneo torneo : torneosCerrados){
-            torneosRegistrados.add(equipoApp.findAll(torneo.getIdTorneo()));
-        }
-        // DAR DE ALTA ENCUENTROS A SUS RESPECTIVOS TORNEOS
-        for ( List<Equipo> torneo : torneosRegistrados ){
+            torneoRegistrado = equipoApp.findAll(torneo.getIdTorneo());
             // CONTAR NUMERO DE EQUIPOS (PAR O IMPAR) PARA CALCULAR LAS JORNADAS
-            Integer numeroEquipos = torneo.size();
+            Integer numeroEquipos = torneoRegistrado.size();
             Integer numeroJornadas = numeroEquipos - 1;
             Integer numeroEncuentros = numeroEquipos * numeroJornadas / 2;
             Integer numeroPartidosJornada = numeroEquipos / 2;
-            //DETERMINAMOS EL PIVOTE
-            Integer equipoPivote = torneo.get(0).getIdEquipo();
             // VERIFICAR NUMERO DE EQUIPOS (PAR O IMPAR) PARA DETERMINAR SI ASIGNAREMOS DESCANSO
             Encuentro encuentro;
             if(numeroEquipos % 2 == 1){
-                // MEZCLAR TODOS CONTRA TODOS INCLUYENDO DESCANSO = NULL
-                //ANADIMOS A LA LISTA DE EQUIPOS UN EQUIPO NULL PARA QUE SE MEZCLE CON LOS DEMAS
-                torneosRegistrados.add(null);
-
-            } //else {
-                // MEZCLAR TODOS CONTRA TODOS SIN DESCANSO
+                torneoRegistrado.add(null);
+            }
+            // MEZCLAR TODOS CONTRA TODOS SIN DESCANSO
             for(int jornada = 0 ; jornada < numeroJornadas ; jornada ++ ){
                 for (int partido = 0 ; partido < numeroPartidosJornada ; partido ++) {
-                    Integer local = ( jornada + partido ) % (numeroJornadas); 
+                    Integer local = ( jornada + partido ) % (numeroJornadas);
                     Integer visitante = (numeroEquipos - 1 + jornada + partido) % (numeroJornadas);
-                    
+
                     //Con esto mantenemos que el ultimo equipo se quede fijo
-                     if (partido == 0){
+                    if (partido == 0){
                         visitante = numeroEquipos - 1;
                     }
-
-                    encuentro = new Encuentro(new Torneo(1),torneo.get(local),torneo.get(visitante));
-
-                        //encuentro = new Encuentro(
-                          //      new Date(),"Generica",
-                            //    new Torneo(1),
-                             //   new Equipo(torneo.get(i).getIdEquipo()),
-                              //  new Equipo(torneo.get(j).getIdEquipo()));
-                    //}
+                    encuentro = new Encuentro(torneo,torneoRegistrado.get(local),torneoRegistrado.get(visitante));
+                    encuentroApp.save(encuentro);
                 }
             }
         }
-
-
     }
 }
